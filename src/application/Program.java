23ee77820ac9e34d3c1f92201e6454d00c972574
Myckamorais <myckael2k@gmail.com>
2026-08-17 20:50:55 -0300

    Create calculate total to product list

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,18 @@ package application;
 
 import entities.Product;
 import enums.CategoriaEnum;
+import service.ProductService;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Program {
     void main(){
 
         List<Product> products = new ArrayList<>();
+        ProductService ps = new ProductService();
 
         products.add(new Product("Mouse", CategoriaEnum.PERIFERICOS, 15, 45.00));
         products.add(new Product("Teclado", CategoriaEnum.PERIFERICOS, 8, 120.00));
@@ -19,7 +23,10 @@ public class Program {
         products.add(new Product("Headset", CategoriaEnum.ACCESSORIOS, 12, 180.00));
         products.add(new Product("SSD 1TB", CategoriaEnum.ELETRONICOS, 20, 430.00));
 
-        
+        double total = ps.totalValue(products);
+        NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        System.out.println(formatoMoeda.format(total));
+
 
     }
 }
--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -2,10 +2,20 @@ package service;
 
 import entities.Product;
 
+import java.util.List;
+
 public class ProductService {
 
-    private Product product;
 
+    public double totalValue(List<Product> list){
+        double total = 0;
+        if (list != null){
+            for (Product x : list){
+                total += x.getQuantity() * x.getPriceUnit();
+            }
+        }
+        return total;
+    }
 
 
 
